538a4ad606ca66f5f266e4a3502c5372308531ce
analogsoul13 <analogsoul13@gmail.com>
2024-09-19 14:43:26 +0530

    ui changes

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -31,7 +31,7 @@ const DisplayHome = () => {
         <div className='mb-4'>
             <h1 className='my-5 font-bold text-2xl'>Popular albums</h1>
             <div className='flex overflow-auto'>
-                {songsData.map((item,index)=>(<SongItem key={index} name={item.name} desc={item.desc} id={item.id} image={item.image} />))}
+                {songsData.map((item,index)=>(<SongItem key={index} name={item.name} id={item.id} image={item.image} />))}
             </div>
         </div>   
       
